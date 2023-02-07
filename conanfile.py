d4c617543fd479b55d1d1d7d982ecd7804077995
Harald Held <harald.held@gmail.com>
2023-02-07 08:25:07 +0100

    Fix recipe to work with latest version of Conan again.

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
-from importlib.metadata import requires
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
 
 
 class EnsmallenConan(ConanFile):
@@ -11,19 +11,24 @@ class EnsmallenConan(ConanFile):
     description = "a high-quality C++ library for non-linear numerical optimization"
     topics = ("optimization", "gradient descent", "gradient free", "machine learning")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
     requires = ["armadillo/10.7.3"]
     default_options = "armadillo:use_hdf5=False"
 
     def source(self):
         self.run(f"git clone -b {self.version} https://github.com/mlpack/ensmallen.git")
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.preprocessor_definitions["ARMADILLO_INCLUDE_DIR"] = self.dependencies["armadillo"].cpp_info.includedirs[0]
+        tc.preprocessor_definitions["ARMADILLO_LIBRARY"] = self.dependencies["armadillo"].cpp_info.libs
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
     def build(self):
-        print(self)
         cmake = CMake(self)
-        cmake.definitions["ARMADILLO_INCLUDE_DIR"] = self.dependencies["armadillo"].cpp_info.includedirs[0]
-        cmake.definitions["ARMADILLO_LIBRARY"] = self.dependencies["armadillo"].cpp_info.libs
-        cmake.configure(source_folder="ensmallen")
+        cmake.configure(build_script_folder="ensmallen")
         cmake.build()
         cmake.install()
 
@@ -31,3 +36,5 @@ class EnsmallenConan(ConanFile):
         self.copy("*.h", dst="include", src="ensmallen")
         self.copy("*.hpp", dst="include", src="ensmallen")
 
+    def layout(self):
+        cmake_layout(self)
